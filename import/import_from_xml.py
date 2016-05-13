cf184c625fb6b1a3204ded00495b2c2cb7bc17a9
vpcsilva <vpcx5@yahoo.com.br>
2016-05-13 07:43:24 -0300

    Refatorando o script de import

--- import/import_from_xml.py
+++ import/import_from_xml.py
@@ -85,81 +85,61 @@ def import_from_file( root, session ):
 
         if numero_documento is not None:
             documento_obj.numero_documento=numero_documento.text
-
         if tipo_documento is not None:
             documento_obj.tipo_documento=tipo_documento.text
-
         if data_emissao_str is not None:
             date_string = re.sub('\.[0-9]+', "", data_emissao_str.text)
             documento_obj.data_emissao=datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S")
-
         if valor_documento is not None:
             documento_obj.valor_documento=float(valor_documento.text)
-
         if valor_glossa is not None:
             documento_obj.valor_glossa=float(valor_glossa.text)
-
         if valor_liquido is not None:
             documento_obj.valor_liquido=float(valor_liquido.text)
-
         if mes is not None:
             documento_obj.mes=int(mes.text)
-
         if ano is not None:
             documento_obj.ano=int(ano.text)
-
         if numero_parcela is not None:
             documento_obj.numero_parcela=numero_parcela.text
-
         if nome_passageiro is not None:
             documento_obj.nome_passageiro=nome_passageiro.text
-
         if trecho is not None:
             documento_obj.trecho=trecho.text
-
         if numero_lote is not None:
             documento_obj.numero_lote=numero_lote.text
-
         if numero_ressarcimento is not None:
             documento_obj.numero_ressarcimento=numero_ressarcimento.text
-
         if valor_restituicao is not None:
             documento_obj.valor_restituicao=float(valor_restituicao.text)
-
         if indentificador_solicitante is not None:
             documento_obj.indentificador_solicitante=indentificador_solicitante.text
-
         if numero_subcota is not None:
             documento_obj.numero_subcota=numero_subcota.text
-
         if descricao_subcota is not None:
             documento_obj.descricao_subcota=descricao_subcota.text
-
         if descricao_subcota is not None:
             documento_obj.num_especificacao_subcota=num_especificacao_subcota.text
-
         if descricao_especificacao_subcota is not None:
             documento_obj.descricao_especificacao_subcota=descricao_especificacao_subcota.text
-
         if legislatura_obj is not None:
             documento_obj.legislatura_fk=legislatura_obj.id
-
         if fornecedor_obj is not None:
             documento_obj.fornecedor_fk=fornecedor_obj.id
-
         session.add(documento_obj)
         session.commit()
 
 print "Iniciando o script..."
 url_ano_atual = "http://www.camara.gov.br/cotas/AnoAtual.zip"
 url_ano_anterior = "http://www.camara.gov.br/cotas/AnoAnterior.zip"
-print "Baixando o arquivo do site da camara..."
-file_ano_atual, _ = urllib.urlretrieve(url_ano_atual)
-file_ano_anterior, _ = urllib.urlretrieve(url_ano_anterior)
+url_demais_anos = "http://www.camara.gov.br/cotas/AnosAnteriores.zip"
+
 
 print "Importando Ano Atual"
+print "Baixando o arquivo..."
+downloaded_file, _ = urllib.urlretrieve(url_ano_atual)
 print "Colocando o arquivo em um objeto..."
-zip_file_object = zipfile.ZipFile(file_ano_atual, 'r')
+zip_file_object = zipfile.ZipFile(downloaded_file, 'r')
 print "Removendo o arquivo interno..."
 first_file = zip_file_object.namelist()[0]
 print "Colocando o arquivo xml em uma variavel..."
@@ -175,8 +155,29 @@ import_from_file( root, session )
 session.close()
 
 print "Importando Ano anterior"
+print "Baixando o arquivo..."
+downloaded_file, _ = urllib.urlretrieve(url_ano_anterior)
 print "Colocando o arquivo em um objeto..."
-zip_file_object = zipfile.ZipFile(file_ano_anterior, 'r')
+zip_file_object = zipfile.ZipFile(downloaded_file, 'r')
+print "Removendo o arquivo interno..."
+first_file = zip_file_object.namelist()[0]
+print "Colocando o arquivo xml em uma variavel..."
+file = zip_file_object.open(first_file)
+print "Abrindo o arquivo XML com o ElementTree..."
+tree = ET.parse(file)
+root = tree.getroot()
+print "Criando a sessao..."
+Session = sessionmaker(engine)
+session = Session()
+print "Criando os objetos a partir de cada objeto no arquivo xml..."
+import_from_file( root, session )
+session.close()
+
+print "Importando demais anos"
+print "Baixando o arquivo..."
+downloaded_file, _ = urllib.urlretrieve(url_demais_anos)
+print "Colocando o arquivo em um objeto..."
+zip_file_object = zipfile.ZipFile(downloaded_file, 'r')
 print "Removendo o arquivo interno..."
 first_file = zip_file_object.namelist()[0]
 print "Colocando o arquivo xml em uma variavel..."
